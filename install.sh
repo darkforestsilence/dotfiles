6a8e0f1fc1b4ac5ea1aa6628808fd747f0c4696c
Amy Larane <amylarane@gmail.com>
2020-11-21 15:36:29 -0600

    Add setup script

---
+++ install.sh
@@ -0,0 +1,24 @@
+cd $HOME
+
+SCRIPT=$(readline -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+# bash setup
+rm -f $HOME/.bashrc
+ln -sv $SCRIPTPATH/bash/bashrc $HOME/.bashrc
+ln -sv $SCRIPTPATH/bash/bash $HOME/.bash
+
+# vim setup
+rm -f $HOME/.vimrc
+rm -rf $HOME/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+ln -sv $SCRIPTPATH/vim/vimrc $HOME/.vimrc
+vim -c 'PluginInstall' -c 'qa!'
+
+# input setup
+rm -f $HOME/.inputrc
+ln -sv $SCRIPTPATH/input/inputrc $HOME/.inputrc
+
+# tmux setup
+rm $HOME/.tmux
+ln -sv $SCIRPTPATH/tmux/tmux.conf $HOME/.tmux.conf
